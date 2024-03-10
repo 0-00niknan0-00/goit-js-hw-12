d6f5af536da0b529ac9bc8ff7ac4d8b33400382d
0-0niknan0-0 <niknan202005@gmail.com>
2024-03-10 16:46:26 +0100

    initial end

---
+++ src/main.js
@@ -0,0 +1,60 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { renderMarcup, showEndOfListMessage } from './js/render-functions';
+import { fetchImages } from './js/pixabay-api';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 150,
+});
+
+const form = document.querySelector('.search-form');
+const container = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-btn');
+let searchWord = '';
+
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSubmit(event) {
+  event.preventDefault();
+  container.innerHTML = '';
+  searchWord = form.elements.searchWord.value.trim();
+  fetchImages(searchWord)
+    .then(data => {
+      const marcup = renderMarcup(data);
+      container.insertAdjacentHTML('beforeend', marcup);
+
+      lightbox.refresh();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  form.reset();
+  loadMoreBtn.style.display = 'block';
+}
+
+async function onLoadMore() {
+  currPage += 1;
+  try {
+    const images = await fetchImages(searchWord, currPage);
+    renderMarcup(images);
+    lightbox.refresh();
+
+    const cardHeight = container.getBoundingClientRect().height;
+    window.scrollBy({
+      top: 4 * cardHeight,
+      behavior: 'smooth',
+    });
+
+    if (!images.hits || images.hits.length === 0) {
+      loadMoreBtn.style.display = 'none';
+      showEndOfListMessage();
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
